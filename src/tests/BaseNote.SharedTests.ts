5982d0c9d84478634d460712d953e044eb21f8ab
Diego <dcarrillozamora.d@gmail.com>
2026-01-02 11:34:16 -0600

    Added the basenote tests for reusability

---
+++ src/tests/BaseNote.SharedTests.ts
@@ -0,0 +1,61 @@
+import {BaseNote} from "../notes/BaseNote.ts";
+import {describe, it, expect, test} from "vitest";
+import { any } from "./anyType.ts";
+
+type Constructor<T> = new (content: string, x_coordinate: number, y_coordinate: number) => T;
+
+export function BaseNoteSharedTests<T extends BaseNote>(NoteClass: Constructor<T>)  {
+    describe('Standard test for all notes derived from BaseNote', () => {
+
+        it("StickyNote loads information", () => {
+            const note = new NoteClass("Information", any<number>(), any<number>());
+
+            expect(note.content).toBe("Information");
+        });
+
+        it("Created at the right time", () =>{
+            const before = Date.now();
+            const note = new NoteClass(any<string>(), any<number>(), any<number>());
+            const after = Date.now();
+
+            expect(note.createAt).toBeGreaterThanOrEqual(before);
+            expect(note.createAt).toBeLessThanOrEqual(after);
+        });
+
+
+        it("Constructor makes unique IDs", () => {
+            const note1 = new NoteClass(any<string>(), any<number>(), any<number>());
+            const note2 = new NoteClass(any<string>(), any<number>(), any<number>());
+
+            expect(note1.id).not.toBe(note2.id);
+        });
+
+        test.each([
+            [0,0],
+            [-1,-1],
+            [1,1],
+            [1,-1],
+            [-1,1],
+        ])("Creates coordinates (%i, %i)", (x,y) => {
+            const note = new NoteClass(any<string>(), x, y);
+
+            expect(note.position.x).toBe(x);
+            expect(note.position.y).toBe(y);
+        });
+
+        test.each([
+            [2,-1],
+            [0.43, 333],
+            [0,0],
+            [-2323333,3333],
+            [-12,-44,]
+        ])("Change the note's coordinates (%i, %i)", (x,y) => {
+            const note = new NoteClass(any<string>(), any<number>(), any<number>());
+
+            note.changeCoordinate(x,y);
+
+            expect(note.position).toEqual({ x, y });
+        });
+
+    });
+}
