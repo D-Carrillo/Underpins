3c7fac86e10310a3147e17b92c761f0e4a059b3c
Diego <dcarrillozamora.d@gmail.com>
2026-01-09 16:19:12 -0600

    Notes are now created in the right place

--- src/menus/BoardMenu.ts
+++ src/menus/BoardMenu.ts
@@ -1,6 +1,7 @@
 import {NotesManager} from "../managers/NoteManager.ts";
 import {NoteTypes} from "../factories/NoteTypesEnum.ts";
 import { invoke } from "@tauri-apps/api/core";
+import {BoardManager} from "../managers/BoardManager.ts";
 
 export function BoardMenu(event: MouseEvent, menu: HTMLDivElement, _: string) {
     const TextNoteNewButton = document.createElement("button");
@@ -10,15 +11,17 @@ export function BoardMenu(event: MouseEvent, menu: HTMLDivElement, _: string) {
     ImageNoteNewButton.textContent = "Add Image Note";
 
     TextNoteNewButton.onclick = () => {
-        NotesManager.createNote(event.pageX, event.pageY, NoteTypes.TEXT, "New Note");
+        const pos = BoardManager.getViewport()?.toLocal(event)!;
+        NotesManager.createNote(pos.x, pos.y, NoteTypes.TEXT, "New Note");
         menu.remove();
     };
 
     ImageNoteNewButton.onclick = async () => {
         try {
             const newImagePath = await invoke('image_setter') as string;
+            const pos = BoardManager.getViewport()?.toLocal(event)!;
 
-            NotesManager.createNote(event.pageX, event.pageY, NoteTypes.IMAGE, newImagePath);
+            NotesManager.createNote(pos.x, pos.y, NoteTypes.IMAGE, newImagePath);
         } catch (error) {
             console.error("Error choosing or copying file", error);
         }
